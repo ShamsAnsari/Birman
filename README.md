7b54c5b9fdf338d6a8bcf009966a07f6fba69afe
Shams Ansari <shamsahmedansari@gmail.com>
2021-10-31 13:25:27 -0700

    :(
    dart/flutter looks kinda hard to learn rip

--- README.md
+++
@@ -1,2 +0,0 @@
-You have to follow to guide to install flutter.
-https://flutter.dev/docs/get-started/install
--- birman/android/app/src/main/kotlin/com/example/birman/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.birman
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
