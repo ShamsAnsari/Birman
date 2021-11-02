19496eea54b6eb79620dbd6b5e831e89bd631f90
Shams Ansari <shamsahmedansari@gmail.com>
2021-11-01 22:32:33 -0700

    testing out random quote API

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function randomQuote() {
+    $.getJSON('https://api.alexx.lol/random/quote', function (data) {
+        console.log(data)
+        document.getElementById("quote-show").innerHTML = data;
+
+
+      
+    }
+
+
+}
