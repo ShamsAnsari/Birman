2ebff52e3b080a3de0e1af2d2844b1a1fec1350c
Shams Ansari <shamsahmedansari@gmail.com>
2021-11-02 19:52:28 -0700

    Moved things around

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 function randomQuote() {
+
     $.getJSON('https://api.alexx.lol/random/quote', function (data) {
         // JSON result in `data` variable
         console.log(data)
-        document.getElementById("quote-show").innerHTML = data;
-        let url = 'https://example.com';
+        document.getElementById("quote-show").innerHTML = data['text'];
 
 
-      
+
     });
-
-
 }
